ffaa218daa0c30336d6037dbb0f48201905f4411
青木𝙰𝚘𝚔𝚒.𝚀𝙼 <3452798215@qq.com>
2026-06-24 00:45:36 +0800

    Add core/chat_config.lua

---
+++ core/chat_config.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+M.WEBHOOK_URL = "https://discord.com/api/webhooks/1517068958149246998/38je8qJPgOiahuLnht_n0gRQhzZyDVJEl2NkZ9P-q7OPDVCIajknD05VG965hntELLYr"
+
+M.ADMIN_DISCORD_ID = "1455508997598875739"
+
+M.CHANNEL_ID = "1517064900713648128"
+
+M.BOT_TOKEN = ""  
+
+M.PROXY_URL = "https://discord-chat-proxy.onrender.com/api/discord-messages"
+
+_G.BFH.Core = _G.BFH.Core or {}; _G.BFH.Core.CHAT_CONFIG = M
+return M
