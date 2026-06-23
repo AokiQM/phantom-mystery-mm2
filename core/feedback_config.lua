8d7ed2cebc64cf695014675fc1b863e063f98b5a
青木𝙰𝚘𝚔𝚒.𝚀𝙼 <3452798215@qq.com>
2026-06-24 00:45:37 +0800

    Add core/feedback_config.lua

---
+++ core/feedback_config.lua
@@ -0,0 +1,8 @@
+local M = {}
+
+M.BUG_WEBHOOK_URL = "https://discord.com/api/webhooks/1517069468231143466/rCl80VMtqW-aIAI2SHgphdKEn38PDgMysghTii5JE3GGrViQENlwjL8vZpgrUBHUUSb1"
+
+M.FEEDBACK_WEBHOOK_URL = "https://discord.com/api/webhooks/1517069684967604294/IIhnH6Txk49xsYu5YVpaz52HjL4MUBnFJC5wRRwrRbvSD8LOyoFcxl_1_buOnbCDmk-i"
+
+_G.BFH.Core = _G.BFH.Core or {}; _G.BFH.Core.FEEDBACK_CONFIG = M
+return M
